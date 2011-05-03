d4660eff200944310c15d971a88e5ab9228b33e3
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2011-05-03 20:36:23 -0300

    Internacionalizando as jsp.

---
+++ src/main/java/com/appspot/simularso/controller/SobreController.java
@@ -0,0 +1,24 @@
+package com.appspot.simularso.controller;
+
+import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Resource;
+
+import com.appspot.simularso.infra.Idioma;
+
+@Resource
+public class SobreController {
+
+	private Idioma idioma;
+
+	public SobreController(Idioma idioma) {
+		this.idioma = idioma;
+	}
+
+	@Get("/sobre/")
+	public void sobre() {
+	}
+
+	public String getIdioma() {
+		return this.idioma.getIdioma();
+	}
+}
