b5010b324375d3c793229a7e0b6740210e50df27
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2011-05-09 13:58:36 -0300

    Correcao de conflitos.

--- src/main/java/com/appspot/simularso/infra/RoutesConfigurator.java
+++ src/main/java/com/appspot/simularso/infra/RoutesConfigurator.java
@@ -1,12 +1,12 @@
 package com.appspot.simularso.infra;
 
-import com.appspot.simularso.controller.MainController;
-
 import br.com.caelum.vraptor.http.route.Router;
 import br.com.caelum.vraptor.http.route.RoutesConfiguration;
 import br.com.caelum.vraptor.http.route.Rules;
 import br.com.caelum.vraptor.ioc.ApplicationScoped;
 import br.com.caelum.vraptor.ioc.Component;
+
+import com.appspot.simularso.controller.HomeController;
 
 @Component
 @ApplicationScoped
@@ -16,8 +16,8 @@ public class RoutesConfigurator implements RoutesConfiguration{
 	public void config(Router rota) {
 		new Rules(rota){
 			public void routes(){
-				routeFor("").is(MainController.class).home();
-				routeFor("/").is(MainController.class).home();
+				routeFor("").is(HomeController.class).home();
+				routeFor("/").is(HomeController.class).home();
 			}
 		};
 	}
